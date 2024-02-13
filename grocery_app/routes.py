49851b9ea095653808b39cdb3fd5d6a1da17fe92
mathyasp <mathyastp@gmail.com>
2024-02-12 23:33:17 -0500

    completed new_store and new_item routes and templates

--- grocery_app/routes.py
+++ grocery_app/routes.py
@@ -21,26 +21,47 @@ def homepage():
 @main.route('/new_store', methods=['GET', 'POST'])
 def new_store():
     # TODO: Create a GroceryStoreForm
-
+    form = GroceryStoreForm()
     # TODO: If form was submitted and was valid:
     # - create a new GroceryStore object and save it to the database,
     # - flash a success message, and
     # - redirect the user to the store detail page.
+    if form.validate_on_submit():
+        new_store = GroceryStore(
+            title=form.title.data,
+            address=form.address.data,
+            items=form.items.data
+        )
+        db.session.add(new_store)
+        db.session.commit()
 
+        flash('New store was created successfully.')
     # TODO: Send the form to the template and use it to render the form fields
-    return render_template('new_store.html')
+    return render_template('new_store.html', form=form)
 
 @main.route('/new_item', methods=['GET', 'POST'])
 def new_item():
     # TODO: Create a GroceryItemForm
-
+    form = GroceryItemForm()
     # TODO: If form was submitted and was valid:
     # - create a new GroceryItem object and save it to the database,
     # - flash a success message, and
     # - redirect the user to the item detail page.
+    if form.validate_on_submit():
+        new_item = GroceryItem(
+            name=form.name.data,
+            price=form.price.data,
+            category=form.category.data,
+            photo_url=form.photo_url.data,
+            store_id=form.store_id.data,
+            store=form.store.data
+        )
+        db.session.add(new_item)
+        db.session.commit()
 
+        flash('New item was created successfully.')
     # TODO: Send the form to the template and use it to render the form fields
-    return render_template('new_item.html')
+    return render_template('new_item.html', form=form)
 
 @main.route('/store/<store_id>', methods=['GET', 'POST'])
 def store_detail(store_id):
